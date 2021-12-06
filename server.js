7b03e64271d8f4b71a36f836644aa5be76fa156d
Nathanael White <narhanaelw99@gmail.com>
2021-12-06 11:39:49 -0600

    more problem solving

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 // const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
+// require authorization from utils/auth later
 const db = require('./config/connection.js');
 
 const PORT = process.env.PORT || 3001
