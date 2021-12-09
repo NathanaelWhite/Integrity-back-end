fea561d6f9838195b2c2e27e6a5adfdb8e94f11f
Nathanael White <narhanaelw99@gmail.com>
2021-12-09 14:21:02 -0600

    editing server.js to accomidate mongo and express only

--- server.js
+++ server.js
@@ -1,41 +1,16 @@
-const express = require('express');
-// const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
 
-// require authorization from utils/auth later
-const db = require('./config/connection.js');
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 const app = express();
-
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    // context: authMiddleware,
-});
-
-async function startup() {
-    await server.start();
-    server.applyMiddleware({ app });
-}
-startup();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../client/build')));
-//   }
-  
-//   // app.use(routes);
-  
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
-//   });
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/integritysandc"
+);
 
-db.once('open', () => {
-    app.listen(PORT, () => {
-        console.log(`API server now on port ${PORT}`);
-        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    });
-})
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}`);
+});
