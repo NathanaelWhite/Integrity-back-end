4a0209c137ea74c9cd47e9fc6c3a3ac8f7b87a1a
Nathanael White <narhanaelw99@gmail.com>
2021-12-08 22:54:13 -0600

    deleting graphql from app

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
 // const path = require('path');
 
-const { typeDefs, resolvers } = require('./schemas');
 // require authorization from utils/auth later
 const db = require('./config/connection.js');
 
