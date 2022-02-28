055130d6141033759201a134d6c2b9a13f8cdfd1
Nathanael White <narhanaelw99@gmail.com>
2022-02-28 16:20:22 -0600

    created initial hero section for homepage with some styling

--- integrity/src/components/Hero/Hero.js
+++ integrity/src/components/Hero/Hero.js
@@ -1,21 +1,47 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
-
+import { Button } from "react-bootstrap";
+import heroImg from "../../assets/images/hero.jpg";
 
 const StyledHero = styled.div`
-    background-color: red;
-`
+  background-image: url("${heroImg}");
+  background-size: cover;
+  background-attachment: fixed;
+  margin: 0;
+  padding: 10px;
+  color: #fff;
+  text-align: center;
+  h1 {
+    font-size: 3rem;
+    margin: 0;
+  }
+  h3 {
+    font-size: 1rem;
+    margin: 0 0 15px 0;
+  }
+  @media (min-width: 768px) {
+    padding: 20px;
+    h1 {
+      font-size: 5rem;
+    }
+    h3 {
+      font-size: 2.5rem;
+    }
+  }
+`;
 
 const Hero = () => {
   return (
     <div>
-        <StyledHero>
-            <h1 className='hero-title'>INTEGRITY</h1>
-            <h3 className='hero-sub-title'>Strength & Conditioning</h3>
-            <button>Get Started Today</button>
-        </StyledHero>
+      <StyledHero>
+        <h1 className="hero-title">INTEGRITY</h1>
+        <h3 className="hero-sub-title">Strength & Conditioning</h3>
+        <Button variant="secondary" className="hero-btn">
+          Get Started Today
+        </Button>
+      </StyledHero>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
