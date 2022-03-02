951bf7f7b0663fec429ac67f1ec105e233c14a41
Nathanael White <narhanaelw99@gmail.com>
2022-03-01 22:59:31 -0600

    created coaches cards

---
+++ integrity/src/components/Team/CoachModal.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CoachModal = () => {
+  return (
+    <div>CoachModal</div>
+  )
+}
+
+export default CoachModal
---
+++ integrity/src/components/Team/data.js
@@ -0,0 +1,20 @@
+export const teamData = [
+    {
+        name: "Brian Kost",
+        bio: "a bunch of text about the man himself",
+        certs: ["CF-L1", "USAW"],
+        socials: ["facebook", "instagram", "email"]
+    },
+    {
+        name: "Nathanael White",
+        bio: "a bio about me or something like that",
+        certs: ["CF-L1", "SRM"],
+        socials: ["facebook", "instagram", "email"]
+    },
+    {
+        name: "Bradley Shanks",
+        bio: "a bio about good ole Brad",
+        certs: ["CF-L1"],
+        socials: ["facebook", "email"]
+    }
+];
