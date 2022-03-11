dddee246184c3eba241d02c342cd1f2434b2c4ff
Nathanael White <narhanaelw99@gmail.com>
2022-03-11 14:23:54 -0600

    added react router and created basic memberships cards

--- integrity/src/App.js
+++ integrity/src/App.js
@@ -4,13 +4,27 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Homepage from "./pages/Homepage";
+import Signup from "./pages/Signup";
+import Memberships from "./pages/Memberships";
+import Schedule from "./pages/Schedule";
+import Faq from "./pages/Faq";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <>
         <Navbar />
-        <Homepage />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/memberships" element={<Memberships />} />
+          <Route path="/schedule" element={<Schedule />} />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
         <Footer />
       </>
     </Router>
--- integrity/src/pages/Memberships.js
+++ integrity/src/pages/Memberships.js
@@ -1,7 +1,63 @@
-import React from 'react'
+import React from "react";
+import { Card, Button } from "react-bootstrap";
 
 export default function memberships() {
   return (
-    <div>memberships</div>
-  )
+    <div>
+      <h1>Membership Options</h1>
+      <Card>
+        <Card.Body>
+          <Card.Title>Unlimited / Month</Card.Title>
+          <Card.Text>$120 - Billed Monthly until cancelled</Card.Text>
+          <Card.Text>
+            Our most common membership option for attending unlimited Group
+            Classes and Open Gym throughout the month.
+          </Card.Text>
+          <Button variant="primary" href="#signup">
+            Select Plan
+          </Button>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>Couples Unlimited / Month</Card.Title>
+          <Card.Text>$170 - Billed Monthly until cancelled</Card.Text>
+          <Card.Text>
+            Membership option for couples to attend unlimited Group Classes and
+            Open Gym throughout the month.
+          </Card.Text>
+          <Button variant="primary" href="#signup">
+            Select Plan
+          </Button>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>3x / Week</Card.Title>
+          <Card.Text>$95 - Billed Monthly until cancelled</Card.Text>
+          <Card.Text>
+            The 3 attendances per week plan allows the member to attend Group
+            Classes and/or Open Gym for up to 3 days per week. The week begins
+            on Sunday and ends on Saturday.
+          </Card.Text>
+          <Button variant="primary" href="#signup">
+            Select Plan
+          </Button>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>Open Gym</Card.Title>
+          <Card.Text>$55 - Billed Monthly until cancelled</Card.Text>
+          <Card.Text>
+            Membership plan for unlimited attendances during scheduled Open Gym
+            hours
+          </Card.Text>
+          <Button variant="primary" href="#signup">
+            Select Plan
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
