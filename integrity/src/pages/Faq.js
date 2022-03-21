cb6c9e5620ad20d2e1e062d9f4101e5f63ee2c16
Rachel Solov <rsolov23@yahoo.com>
2022-03-21 16:57:24 -0500

    added to faq component

--- integrity/src/pages/Faq.js
+++ integrity/src/pages/Faq.js
@@ -1,7 +1,100 @@
-import React from 'react'
-
+import React from "react";
+import { Container, ListGroup } from "react-bootstrap";
 export default function Faq() {
   return (
-    <div>Faq</div>
-  )
+    <>
+      <h1 className="text-center mb-5">Frequently Asked Questions(FAQ)</h1>
+      <Container className="p-10">
+        <ListGroup className="p-10">
+          <ListGroup.Item action variant="danger">
+            <h2>Q.</h2>
+          </ListGroup.Item>
+
+          <ListGroup.Item action variant="light" className="mb-5">
+            <h2>A.</h2>
+          </ListGroup.Item>
+        </ListGroup>
+
+        <ListGroup className="p-10">
+          <ListGroup.Item action variant="danger">
+            <h2>Q.</h2>
+          </ListGroup.Item>
+
+          <ListGroup.Item action variant="light" className="mb-5">
+            <h2>A.</h2>
+          </ListGroup.Item>
+        </ListGroup>
+
+        <ListGroup className="p-10">
+          <ListGroup.Item action variant="danger">
+            <h2>Q.</h2>
+          </ListGroup.Item>
+
+          <ListGroup.Item action variant="light" className="mb-5">
+            <h2>A.</h2>
+          </ListGroup.Item>
+        </ListGroup>
+
+        <ListGroup className="p-10">
+          <ListGroup.Item action variant="danger">
+            <h2>Q.</h2>
+          </ListGroup.Item>
+
+          <ListGroup.Item action variant="light" className="mb-5">
+            <h2>A.</h2>
+          </ListGroup.Item>
+        </ListGroup>
+
+        <ListGroup className="p-10">
+          <ListGroup.Item action variant="danger">
+            <h2>Q.</h2>
+          </ListGroup.Item>
+
+          <ListGroup.Item action variant="light" className="mb-5">
+            <h2>A.</h2>
+          </ListGroup.Item>
+        </ListGroup>
+
+        <ListGroup className="p-10">
+          <ListGroup.Item action variant="danger">
+            <h2>Q.</h2>
+          </ListGroup.Item>
+
+          <ListGroup.Item action variant="light" className="mb-5">
+            <h2>A.</h2>
+          </ListGroup.Item>
+        </ListGroup>
+
+        <ListGroup className="p-10">
+          <ListGroup.Item action variant="danger">
+            <h2>Q.</h2>
+          </ListGroup.Item>
+
+          <ListGroup.Item action variant="light" className="mb-5">
+            <h2>A.</h2>
+          </ListGroup.Item>
+        </ListGroup>
+
+        <ListGroup className="p-10">
+          <ListGroup.Item action variant="danger">
+            <h2>Q.</h2>
+          </ListGroup.Item>
+
+          <ListGroup.Item action variant="light" className="mb-5">
+            <h2>A.</h2>
+          </ListGroup.Item>
+        </ListGroup>
+
+        <ListGroup className="p-10">
+          <ListGroup.Item action variant="danger">
+            <h2>Q.</h2>
+          </ListGroup.Item>
+
+          <ListGroup.Item action variant="light" className="mb-5">
+            <h2>A.</h2>
+          </ListGroup.Item>
+        </ListGroup>
+      </Container>
+    </>
+  );
 }
