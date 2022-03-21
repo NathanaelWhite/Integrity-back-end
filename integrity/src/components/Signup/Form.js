47bf6bf620f7552205b6242abbaec8c7faa6df76
Nathanael White <narhanaelw99@gmail.com>
2022-03-21 18:32:10 -0500

    eleminated some extra form code

--- integrity/src/components/Signup/Form.js
+++ integrity/src/components/Signup/Form.js
@@ -26,8 +26,7 @@ const SignupForm = () => {
               <option value="2">Female</option>
             </Form.Select>
           </Form.Group>
-        </Row>
-        <Row className="mb-3">
+
           <Form.Group as={Col} md="4">
             <Form.Label>Email</Form.Label>
             <Form.Control required type="email" placeholder="Email" />
@@ -40,8 +39,7 @@ const SignupForm = () => {
             <Form.Label>Phone</Form.Label>
             <Form.Control required type="phone" placeholder="Phone" />
           </Form.Group>
-        </Row>
-        <Row className="mb-3">
+
           <Form.Group as={Col} md="6">
             <Form.Label>Emergency Contact</Form.Label>
             <Form.Control required type="text" placeholder="Name" />
@@ -50,8 +48,7 @@ const SignupForm = () => {
             <Form.Label>Emergency Contact Number</Form.Label>
             <Form.Control required type="text" placeholder="Number" />
           </Form.Group>
-        </Row>
-        <Row className="mb-3">
+
           <Form.Group as={Col} md="6">
             <Form.Label>Date of Birth</Form.Label>
             <Form.Control required type="date" placeholder="DOB" />
@@ -69,8 +66,7 @@ const SignupForm = () => {
               <option value="7">Other</option>
             </Form.Select>
           </Form.Group>
-        </Row>
-        <Row className="mb-3">
+
           <Form.Group as={Col} md="6" controlId="validationCustom01">
             <Form.Label>Address 1</Form.Label>
             <Form.Control required type="text" placeholder="Address 1" />
@@ -80,8 +76,7 @@ const SignupForm = () => {
             <Form.Label>Address 2</Form.Label>
             <Form.Control required type="text" placeholder="Address 2" />
           </Form.Group>
-        </Row>
-        <Row className="mb-3">
+
           <Form.Group as={Col} md="4" controlId="validationCustom01">
             <Form.Label>City</Form.Label>
             <Form.Control required type="text" placeholder="City" />
@@ -94,42 +89,35 @@ const SignupForm = () => {
             <Form.Label>Postal code</Form.Label>
             <Form.Control required type="text" placeholder="Postal code" />
           </Form.Group>
-        </Row>
-        <Row>
+
           <Form.Group as={Col} md="8">
             <Form.Label>
               Integrity Strength & Conditioning Liability Waiver
             </Form.Label>
           </Form.Group>
-        </Row>
-        <Row className="mb-3">
+
           <textarea readOnly>{waiverText}</textarea>
-        </Row>
-        <Row className="mb-3">
+
           <Form.Label>* Please use your mouse/finger to sign name</Form.Label>
           <Canvas />
-        </Row>
-        <Row className="m-3">
+
           <Form.Check
             type="checkbox"
             id="signature-checkbox"
             label="By clicking this checkbox you agree to online signature signing of this waiver *"
           />
-        </Row>
-        <Row className="mb-3">
+
           <Form.Group as={Col} md="6">
             <Form.Label>Enter Credit or Debit Card Information *</Form.Label>
             <Form.Control required type="text" placeholder="CARD NUMBER" />
           </Form.Group>
-        </Row>
-        <Row className="m-3">
+
           <Form.Check
             type="checkbox"
             id="consent-checkbox"
             label="I consent to conduct electronic business *"
           />
-        </Row>
-        <Row className="mb-3">
+
           <Form.Group as={Col} md="4">
             <Form.Label>Please confirm *</Form.Label>
             <Form.Check
@@ -138,8 +126,7 @@ const SignupForm = () => {
               label="reCAPTCHA"
             />
           </Form.Group>
-        </Row>
-        <Row className="mb-5">
+
           <Button as={Col} md="12">
             Sign Up
           </Button>
