dc877ad1427354a95756f1d16d99a9b1eb231361
Nathanael White <narhanaelw99@gmail.com>
2022-03-22 13:45:18 -0500

    cleaned up signup form and changed hero img

--- integrity/src/components/Signup/Form.js
+++ integrity/src/components/Signup/Form.js
@@ -12,12 +12,10 @@ const SignupForm = () => {
             <Form.Label>First Name</Form.Label>
             <Form.Control required type="text" placeholder="First Name" />
           </Form.Group>
-
           <Form.Group as={Col} md="4" controlId="validationCustom02">
             <Form.Label>Last Name</Form.Label>
             <Form.Control required type="text" placeholder="Last Name" />
           </Form.Group>
-
           <Form.Group as={Col} md="4" controlId="validationCustomGender">
             <Form.Label>Gender</Form.Label>
             <Form.Select required aria-label="Default select example">
@@ -26,7 +24,6 @@ const SignupForm = () => {
               <option value="2">Female</option>
             </Form.Select>
           </Form.Group>
-
           <Form.Group as={Col} md="4">
             <Form.Label>Email</Form.Label>
             <Form.Control required type="email" placeholder="Email" />
@@ -39,7 +36,6 @@ const SignupForm = () => {
             <Form.Label>Phone</Form.Label>
             <Form.Control required type="phone" placeholder="Phone" />
           </Form.Group>
-
           <Form.Group as={Col} md="6">
             <Form.Label>Emergency Contact</Form.Label>
             <Form.Control required type="text" placeholder="Name" />
@@ -48,7 +44,6 @@ const SignupForm = () => {
             <Form.Label>Emergency Contact Number</Form.Label>
             <Form.Control required type="text" placeholder="Number" />
           </Form.Group>
-
           <Form.Group as={Col} md="6">
             <Form.Label>Date of Birth</Form.Label>
             <Form.Control required type="date" placeholder="DOB" />
@@ -66,17 +61,14 @@ const SignupForm = () => {
               <option value="7">Other</option>
             </Form.Select>
           </Form.Group>
-
           <Form.Group as={Col} md="6" controlId="validationCustom01">
             <Form.Label>Address 1</Form.Label>
             <Form.Control required type="text" placeholder="Address 1" />
           </Form.Group>
-
           <Form.Group as={Col} md="6" controlId="validationCustom02">
             <Form.Label>Address 2</Form.Label>
             <Form.Control required type="text" placeholder="Address 2" />
           </Form.Group>
-
           <Form.Group as={Col} md="4" controlId="validationCustom01">
             <Form.Label>City</Form.Label>
             <Form.Control required type="text" placeholder="City" />
@@ -89,35 +81,28 @@ const SignupForm = () => {
             <Form.Label>Postal code</Form.Label>
             <Form.Control required type="text" placeholder="Postal code" />
           </Form.Group>
-
           <Form.Group as={Col} md="8">
             <Form.Label>
               Integrity Strength & Conditioning Liability Waiver
             </Form.Label>
           </Form.Group>
-
           <textarea readOnly>{waiverText}</textarea>
-
           <Form.Label>* Please use your mouse/finger to sign name</Form.Label>
           <Canvas />
-
           <Form.Check
             type="checkbox"
             id="signature-checkbox"
             label="By clicking this checkbox you agree to online signature signing of this waiver *"
           />
-
           <Form.Group as={Col} md="6">
             <Form.Label>Enter Credit or Debit Card Information *</Form.Label>
             <Form.Control required type="text" placeholder="CARD NUMBER" />
           </Form.Group>
-
           <Form.Check
             type="checkbox"
             id="consent-checkbox"
             label="I consent to conduct electronic business *"
           />
-
           <Form.Group as={Col} md="4">
             <Form.Label>Please confirm *</Form.Label>
             <Form.Check
@@ -126,7 +111,6 @@ const SignupForm = () => {
               label="reCAPTCHA"
             />
           </Form.Group>
-
           <Button as={Col} md="12">
             Sign Up
           </Button>
