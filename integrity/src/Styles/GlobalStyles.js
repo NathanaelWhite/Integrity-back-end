cd4fde7b4659fbe802a38d241b56953d2d239670
Nathanael White <narhanaelw99@gmail.com>
2022-03-27 20:48:23 -0500

    initial global styles

---
+++ integrity/src/Styles/GlobalStyles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const GlobalStyles = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800&display=swap");
+  font-family: "Montserrat", sans-serif;
+`;
+
+export default GlobalStyles;
