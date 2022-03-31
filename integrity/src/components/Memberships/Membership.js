fea8cef955d7985fe2bcad8c2c592a858ee4f1ca
Nathanael White <narhanaelw99@gmail.com>
2022-03-30 22:23:34 -0500

    initial styling for memberships page

--- integrity/src/components/Memberships/Membership.js
+++ integrity/src/components/Memberships/Membership.js
@@ -1,27 +1,47 @@
-import React from 'react'
+import React from "react";
 import { Card, Button } from "react-bootstrap";
-import memData from './membershipData';
+import memData from "./membershipData";
+import StyledButton from "../../Styles/StyledButton";
+import styled from "styled-components";
+import heroImg from "../../assets/images/hero.jpg";
+
+const StyledMemberships = styled.div`
+  .mem-hero {
+    height: 200px;
+    color: #ffff;
+    background-image: url(${heroImg});
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
 const Membership = () => {
   return (
     <div>
-    <h1>Membership Options</h1>
-    {memData.map((membership, key) => (
-    <Card key={key}>
-      <Card.Body>
-        <Card.Title>{membership.type}</Card.Title>
-        <Card.Text>{membership.price}</Card.Text>
-        <Card.Text>
-          {membership.description}
-        </Card.Text>
-        <Button variant="primary" href="/signup" id={key}>
-          Select Plan
-        </Button>
-      </Card.Body>
-    </Card>
-    ))}
-  </div>
-  )
-}
+      <StyledMemberships>
+        <div className="mem-hero">
+          <h1 className="fw-bold">PRICING</h1>
+          {/* <p>Join our Community.</p> */}
+        </div>
+        {memData.map((membership, key) => (
+          <Card key={key}>
+            <Card.Body>
+              <Card.Title>{membership.type}</Card.Title>
+              <Card.Text>{membership.price}</Card.Text>
+              <Card.Text>{membership.description}</Card.Text>
+              <StyledButton>
+                <Button className="button custom-btn btn-6" type="submit">
+                  Select Plan
+                </Button>
+              </StyledButton>
+            </Card.Body>
+          </Card>
+        ))}
+      </StyledMemberships>
+    </div>
+  );
+};
 
-export default Membership
+export default Membership;
