07e87337b09a8d8e2a10d3e08f456150188bcee4
Nathanael White <narhanaelw99@gmail.com>
2022-04-06 09:29:36 -0500

    styling signup page and canvas. Signup almost done but still needs a background

--- integrity/src/components/Signup/Canvas.js
+++ integrity/src/components/Signup/Canvas.js
@@ -2,6 +2,13 @@ import React from "react";
 import SignatureCanvas from "react-signature-canvas";
 import { Button } from "react-bootstrap";
 import StyledButton from "../../Styles/StyledButton";
+import styled from "styled-components";
+
+const StyledCanvas = styled.div`
+  border: 2px solid #040404;
+  padding: 0;
+  background-color: #fff;
+`;
 
 const Canvas = () => {
   let sigPad = {};
@@ -9,19 +16,23 @@ const Canvas = () => {
     sigPad.clear();
   };
   return (
-    <div className="sigCanvas">
-      <SignatureCanvas
-        canvasProps={{ width: 300, height: 200 }}
-        ref={(ref) => {
-          sigPad = ref;
-        }}
-      />
-      <StyledButton>
+    <>
+      <StyledCanvas>
+        <div className="sigCanvas">
+          <SignatureCanvas
+            canvasProps={{ width: 300, height: 200 }}
+            ref={(ref) => {
+              sigPad = ref;
+            }}
+          />
+        </div>
+      </StyledCanvas>
+      <StyledButton className="p-0 m-1">
         <Button className="button custom-btn btn-6" onClick={clear}>
           Clear Signature
         </Button>
       </StyledButton>
-    </div>
+    </>
   );
 };
 
